f72e10cb00724db744bf0d3a504e2f15f6aa49da
Abby <abbonuan@gmail.com>
2023-07-06 14:45:56 +0800

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='bg-neutral-100 py-4 pl-5'>
-      <h1 className='font-bold text-xl'>Convert Tweet to URL</h1>
+      <h1 className='font-bold text-xl'>Generate Tweet URL</h1>
     </div>
   )
 }
